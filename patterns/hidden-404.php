74a923ed6e4343b2c936e3c0691082aea53e1fad
tyson <83587481+tysonlmao@users.noreply.github.com>
2023-12-22 21:07:31 +1100

    theme default style changes

--- patterns/hidden-404.php
+++ patterns/hidden-404.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Title: 404
  * Slug: baryte/404
@@ -12,7 +13,8 @@
 ?>
 <!-- wp:heading {"textAlign":"center","fontSize":"x-large"} -->
 <h2 class="wp-block-heading has-text-align-center has-x-large-font-size">
-	<?php esc_html_e( 'Page not found', 'baryte' ); ?></h2>
+	<?php esc_html_e('Page not found', 'baryte'); ?></h2>
 <!-- /wp:heading -->
-<!-- wp:paragraph --><p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'baryte' ); ?></p><!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p><?php esc_html_e('It looks like nothing was found at this location. Maybe try a search?', 'baryte'); ?></p><!-- /wp:paragraph -->
 <!-- wp:search {"label":"Search","showLabel":false,"buttonText":"Search"} /-->
